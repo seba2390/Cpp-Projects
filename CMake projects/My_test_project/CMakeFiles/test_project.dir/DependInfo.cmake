1346be909ec414ee60cab2bd1b629052690bc48f
seba2390 <madsen3008@gmail.com>
2022-05-16 02:46:50 +0200

    Soduko project update

---
+++ CMake projects/My_test_project/CMakeFiles/test_project.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sebastianyde/OneDrive - Aarhus Universitet/Div. C++ projecter/CMake projects/My_test_project/src/main.cpp" "CMakeFiles/test_project.dir/src/main.cpp.o" "gcc" "CMakeFiles/test_project.dir/src/main.cpp.o.d"
+  "/Users/sebastianyde/OneDrive - Aarhus Universitet/Div. C++ projecter/CMake projects/My_test_project/src/my_lib/myfunctions.cpp" "CMakeFiles/test_project.dir/src/my_lib/myfunctions.cpp.o" "gcc" "CMakeFiles/test_project.dir/src/my_lib/myfunctions.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Sudoku/build/CMakeFiles/test_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_project.dir/src/main.cpp.o"
+  "CMakeFiles/test_project.dir/src/main.cpp.o.d"
+  "bin/test_project"
+  "bin/test_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
